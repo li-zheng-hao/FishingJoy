c510b1b92278a93b39a13590d9274f68462af34d
lizhenghao <1263212577@qq.com>
2017-08-19 22:26:37 +0800

    8-19

--- FishNet.cpp
+++ FishNet.cpp
@@ -6,6 +6,6 @@ bool FishNet::init()
 	auto aniamte = Animate::create(animation);
 	this->runAction(aniamte);
 	auto sprite = Sprite::createWithSpriteFrame(SpriteFrameCache::getInstance()->getSpriteFrameByName(STATIC_DATA_STRING("net11")));
-	this->setContentSize(sprite->getContentSize());//将渔网的大小设置为最后一张图片的大小
+	this->setContentSize(sprite->getContentSize());//将渔网的大小设置为最后一张图片的大小，也就是最大的那张图
 	return true;
 }
